b87d1905e47d15d988cbbfeb49bea10524f3824c
jirbyt <jirbyt@yandex.ru>
2020-10-05 18:57:46 +0300

    init: start morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+   let decodeStr = expr;
+let arrRes=[];
+let arrWord = [];
+let countWord = expr.length/10;
+
+for (let i=0; i<countWord; i++){
+arrWord[i]=decodeStr.slice(0,10);
+decodeStr=decodeStr.slice(10)
+}
+for(let j=0; j<arrWord.length; j++){
+
+	if (arrWord[j]==='**********'){
+	arrRes[j]= ' ';
+	}
+
+	else{
+		let wordRes = '';
+		let wordTemp = '';
+		let word = arrWord[j];
+		for(let k=0; k<5; k++){
+		wordTemp = word.slice(0,2);
+		if(wordTemp ==='11'){wordRes+='-'}
+		else if (wordTemp ==='10'){wordRes+='.'}
+		else {wordRes+=''};
+		word = word.slice(2);	
+		}
+	arrRes[j] =MORSE_TABLE[wordRes];
+	}
+}
+
+return arrRes.join('');
 }
 
 module.exports = {
